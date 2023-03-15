15b07181c63636239469ff95ccd2dd2a2672fa36
Dmabl <sdfghj@dg.jnb>
2023-03-15 14:53:54 +0100

    First and second activities

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myfirstproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
@@ -16,7 +17,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun openSecondActivity(){
-
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
 
     }
 }
