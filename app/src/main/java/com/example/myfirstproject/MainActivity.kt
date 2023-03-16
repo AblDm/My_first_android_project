82439400cdc694e7e1bea8567d2fc244280239fc
Dmabl <sdfghj@dg.jnb>
2023-03-17 00:56:10 +0100

    First and second activities

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.example.myfirstproject
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val text: TextView = findViewById(R.id.intro)
-        text.setOnClickListener(){
+        text.setOnClickListener{
+            openSecondActivity()
+        }
+
+        val buttun: Button = findViewById(R.id.button)
+        buttun.setOnClickListener{
             openSecondActivity()
         }
     }
